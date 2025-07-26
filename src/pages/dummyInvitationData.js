346444f50a38b984c8e9062087b6a80e97e171f0
Unikgati <unikgati@gmail.com>
2025-07-26 10:40:53 +0800

    Update dummy data.

--- src/pages/dummyInvitationData.js
+++ src/pages/dummyInvitationData.js
@@ -2,42 +2,42 @@
 // Gunakan key sesuai placeholder di template HTML
 
 const dummyInvitationData = {
-  resepsiMaps: 'https://maps.app.goo.gl/hApRVADk9KW4WBcX7',
-  akadMaps: 'https://maps.app.goo.gl/hApRVADk9KW4WBcX7',
-  groomFather: 'Munawir, S.Pd',
+  resepsiMaps: 'https://maps.app.goo.gl/XyZabc123TestLink',
+  akadMaps: 'https://maps.app.goo.gl/TestAkadLocation987',
+  groomFather: 'Budi Santoso',
   weddingGifts: [
     {
-      holder: 'Syariful Anam',
-      name: 'GoPay',
-      account: '089679608282',
+      holder: 'Raka Pratama',
+      name: 'OVO',
+      account: '081234567890',
       type: 'ewallet'
     },
     {
-      name: 'SMBC',
-      holder: 'Syariful Anam',
-      account: '90310316625',
+      name: 'BCA',
+      holder: 'Raka Pratama',
+      account: '1234567890',
       type: 'bank'
     }
   ],
-  akadTimezone: 'WITA',
-  brideMother: 'Rabiatul Adawiyah',
-  akadDate: '2025-07-29',
-  resepsiTime: '11:00',
+  akadTimezone: 'WIB',
+  brideMother: 'Lestari Wulandari',
+  akadDate: '2025-08-15',
+  resepsiTime: '13:30',
   status: 'pending',
-  groomFullName: 'Syariful Anam',
-  selectedMusicId: '58764OPsFOWCx2H6GYKO',
-  brideFullName: 'Siti Aisyah',
-  resepsiDate: '2025-07-29',
-  createdAt: '2025-07-18T22:40:54.284Z',
-  akadLocation: 'Masjid Mahalluttaqwa Dusun Tandek',
-  resepsiLocation: 'Rumah Mempelai Pria',
-  resepsiTimezone: 'WITA',
-  templateId: 'zI60IZ2b9ZZswdnmFrRA',
-  brideNickName: 'Dini',
-  groomMother: 'Fatihatun Wahidah',
-  akadTime: '09:00',
-  brideFather: 'Sofyan Hadi',
-  groomNickName: 'Anam'
+  groomFullName: 'Raka Pratama',
+  selectedMusicId: 'TEST123musicID',
+  brideFullName: 'Aulia Rahma',
+  resepsiDate: '2025-08-15',
+  createdAt: '2025-07-20T15:00:00.000Z',
+  akadLocation: 'Masjid Al-Hikmah, Jakarta Selatan',
+  resepsiLocation: 'Gedung Serbaguna Cempaka Putih',
+  resepsiTimezone: 'WIB',
+  templateId: 'TEMPLATE123456',
+  brideNickName: 'Lia',
+  groomMother: 'Sulastri Handayani',
+  akadTime: '10:00',
+  brideFather: 'Hendri Rahman',
+  groomNickName: 'Raka'
 };
 
 export default dummyInvitationData;
